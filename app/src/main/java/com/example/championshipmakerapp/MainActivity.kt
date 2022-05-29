54076befd4cd65278a6790b11d3f54b8115f8a06
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-05-29 16:41:23 -0300

    test/testing the develop branch

--- app/src/main/java/com/example/championshipmakerapp/MainActivity.kt
+++ app/src/main/java/com/example/championshipmakerapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.championshipmakerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Develop - main branch
+//Each new feature will be designed in a separated branch
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
